d9b906ac6b2475274303bd96ae5e7c3e9a5a2a8f
jmeisele <jmeisele@yahoo.com>
2025-06-27 12:40:05 +0000

    :zap:

--- bedrock_guardrails.tf
+++ bedrock_guardrails.tf
@@ -9,3 +9,104 @@ resource "aws_bedrock_model_invocation_logging_configuration" "this" {
     }
   }
 }
+
+resource "aws_bedrock_guardrail" "this" {
+  name                      = "ai-travel-agent-assistant-guardrail"
+  blocked_input_messaging   = "Sorry, I can not respond to this. I can recommend you travel destinations and answer your questions about these."
+  blocked_outputs_messaging = "Sorry, I can not respond to this. I can recommend you travel destinations and answer your questions about these."
+  description               = "Only respond to the travel recommendation questions, is protected against the most common prompt mis-use threads, provides content moderation, and doesn't answer to competitor's references."
+
+  content_policy_config {
+    filters_config {
+      input_strength  = "HIGH"
+      output_strength = "HIGH"
+      type            = "HATE"
+    }
+    filters_config {
+      input_strength  = "HIGH"
+      output_strength = "HIGH"
+      type            = "SEXUAL"
+    }
+    filters_config {
+      input_strength  = "HIGH"
+      output_strength = "HIGH"
+      type            = "VIOLENCE"
+    }
+    filters_config {
+      input_strength  = "HIGH"
+      output_strength = "HIGH"
+      type            = "INSULTS"
+    }
+    filters_config {
+      input_strength  = "HIGH"
+      output_strength = "HIGH"
+      type            = "MISCONDUCT"
+    }
+    filters_config {
+      input_strength  = "HIGH"
+      output_strength = "NONE"
+      type            = "PROMPT_ATTACK"
+    }
+  }
+
+  sensitive_information_policy_config {
+    pii_entities_config {
+      type   = "NAME"
+      action = "BLOCK"
+    }
+    pii_entities_config {
+      type   = "AGE"
+      action = "ANONYMIZE"
+    }
+
+    regexes_config {
+      action      = "BLOCK"
+      description = "example regex"
+      name        = "regex_example"
+      pattern     = "^\\d{3}-\\d{2}-\\d{4}$"
+    }
+  }
+
+  topic_policy_config {
+    topics_config {
+      name = "Finance"
+      examples = [
+        "What are the cheapest rates?",
+        "Where can I invest to get rich?",
+        "I want a refund!"
+      ]
+      type       = "DENY"
+      definition = "Investment advice refers to inquiries, guidance, or recommendations regarding the management or allocation of funds or assets with the goal of generating returns ."
+    }
+    topics_config {
+      name = "Politics"
+      examples = [
+        "What is the political situation in that country?",
+        "Give me a list of destinations governed by the greens"
+      ]
+      type       = "DENY"
+      definition = "Statements or questions about politics or politicians"
+    }
+  }
+
+  word_policy_config {
+    managed_word_lists_config {
+      type = "PROFANITY"
+    }
+    words_config {
+      text = "SeaScanner"
+    }
+    words_config {
+      text = "Megatravel Deals"
+    }
+    words_config {
+      text = "Expedia"
+    }
+  }
+}
+
+resource "aws_bedrock_guardrail_version" "this" {
+  description   = "example"
+  guardrail_arn = aws_bedrock_guardrail.this.guardrail_arn
+  skip_destroy  = false
+}
